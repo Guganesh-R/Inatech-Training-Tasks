416dd622de0c8307abc20fe2b6a4ebac6e0571fb
Guganesh-R <111949367+Guganesh-R@users.noreply.github.com>
2023-01-25 09:12:33 +0530

    Add files via upload

---
+++ Tasks1/Quest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks1
+{
+    internal class Quest1
+    {
+        public static void Main(string[] args)
+        {
+            double cost;
+            Console.WriteLine("ENTER NUMBER OF UNITS CONSUMED");
+            double units = Convert.ToDouble(Console.ReadLine());
+            if (units <= 200)
+            {
+                cost = units * 2;
+                Console.WriteLine("the cost of power consumption is {0}", cost);
+            }
+            else if (units > 200 && units <= 350)
+            {
+                cost = 400 + ((units - 200) * 3);
+                Console.WriteLine("the cost of power consumption is {0}", cost);
+            }
+            else if (units > 350 && units <= 500)
+            {
+                cost = 400 + 447 + ((units - 350) * 5);
+                Console.WriteLine("the cost of power consumption is {0}", cost);
+            }
+            else if (units > 500)
+            {
+                cost = 400 + 447 + 745 + ((units - 500) * 7);
+                Console.WriteLine("the cost of power consumption is { 0 }", cost);
+            }
+        }
+    }
+}
---
+++ Tasks1/Quest2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks1
+{
+    internal class Quest2
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Do you have any travel history  Yes:No");
+            string ans = Console.ReadLine();
+            if (ans == "Yes" | ans == "YEs" | ans == "YES" | ans == "yES" | ans == "yeS" | ans=="yes")
+            {
+                Console.WriteLine("Do you have temperature?  Yes:No");
+                string ans1 = Console.ReadLine();
+                if (ans1 == "Yes" | ans1 == "YEs" | ans1 == "YES" | ans1 == "yES" | ans1 == "yeS" | ans1 == "yes")
+                {
+                    Console.WriteLine("Do you have Cough or sneez   Yes:No");
+                    string ans2 = Console.ReadLine();
+                    if (ans2 == "Yes" | ans2 == "YEs" | ans2 == "YES" | ans2 == "yES" | ans2 == "yeS" | ans2 == "yes")
+                    {
+                        Console.WriteLine("Take a SWAB TEST");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Quarantine Yourself and take Fever medicines");
+                    }
+                }
+                else
+                {       Console.WriteLine("Home Qurantine for 28 Days");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not COVID-19");
+            }
+        }
+    }
+}
