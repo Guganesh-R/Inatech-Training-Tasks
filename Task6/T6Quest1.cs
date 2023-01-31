f544b4b4e9ea42f50a8a96ba1d79a2e06ebddb5d
Guganesh-R <111949367+Guganesh-R@users.noreply.github.com>
2023-01-31 22:57:07 +0530

    Add files via upload

---
+++ Task6/T6Quest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class T6Quest1
+    {
+        public void readfDes()
+        {
+
+            FileStream Restar = new FileStream("C:\\Inatech Training\\VS Console app\\Guganesh1\\Deserts.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(Restar);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        
+        }
+        public static void Main(string[] args)
+        {
+
+            T6Quest1 obj = new T6Quest1();
+            ArrayList deserts = new ArrayList();
+            Console.WriteLine("Enter the no.of.desserts to be added");
+            int d = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < d; i++)
+            {
+
+                string des = Console.ReadLine();
+                deserts.Add(des);
+            
+            }
+            FileStream Restar = new FileStream(@"C:\Inatech Training\VS Console app\Guganesh1\Deserts.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw1 = new StreamWriter(Restar);
+            foreach (object o in deserts)
+            {
+
+                sw1.Write(o);
+            
+            }
+            sw1.Flush();
+            sw1.Close();
+            Restar.Close();
+
+            obj.readfDes();
+            FileInfo infoco = new FileInfo("C:\\Inatech Training\\VS Console app\\Guganesh1\\Deserts.txt");
+            Console.WriteLine("size of file {0}", infoco.Length);
+            Console.WriteLine("creation time{0}", infoco.CreationTime);
+
+
+        }
+    }
+}
---
+++ Task6/T6Quest2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class T6Quest2
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+        public string? Empgender { get; set; }
+        public int Empage { get; set; }
+
+        public void readfEmp()
+        {
+            FileStream emp = new FileStream("C:\\Inatech Training\\VS Console app\\Guganesh1Emp.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(emp);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        }
+        public static void Main(string[] args)
+        {
+            T6Quest2 emp = new T6Quest2();
+            Console.WriteLine("Enter employee name:");
+            emp.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter employee gender:");
+            emp.Empgender = Console.ReadLine();
+            Console.WriteLine("Enter employee ID:");
+            emp.EmpId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee age:");
+            emp.Empage = Convert.ToInt32(Console.ReadLine());
+
+            FileStream Emp = new FileStream(@"C:\Inatech Training\VS Console app\Guganesh1Emp.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(Emp);
+            sw.Write(emp.EmpName);
+            sw.Write(emp.Empgender);
+            sw.Write(emp.Empage);
+            sw.Write(emp.EmpId);
+            sw.Flush();
+            sw.Close();
+            Emp.Close();
+            emp.readfEmp();
+        }
+    }
+}
