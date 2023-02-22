1695947f3b21ccd23fce22a7b868a8ad91ca37c9
Guganesh-R <111949367+Guganesh-R@users.noreply.github.com>
2023-02-22 19:11:17 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudId { get; set; }
+        public string ? StudName { get; set; }
+        
+        [ForeignKey("Standard")]
+
+        public int refID { get; set; }  
+
+        public Standard Standard { get; set; }
+    }
+    public class Standard 
+    {
+        [Key]
+        public int StdId { get; set; }
+        public int Standname { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using EFConsole.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+    internal class ProductContext:DbContext
+    {
+        private const string connectionstring = "Server=(localdb)\\mssqllocaldb; Database=Productcore; Trusted_Connection=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBatch> ProductBatchs { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> standards { get; set; }
+    }
+}
